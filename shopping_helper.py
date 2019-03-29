4cbf45494baba9eb7ef64207de22790115568f7d
Harsukh Singh <harsukh.singh@gm.com>
2019-03-29 01:40:47 -0400

    final commit

--- shopping_helper.py
+++ shopping_helper.py
@@ -31,7 +31,6 @@ import copy
 import json
 
 
-minimum_number_to_visit = list()
 # to help you get started, we have provided some boiler plate code
 def satisfy_shopping_list(shopping_list_json, inventory_json):
     # find out minimum combination of stores that would satisfy shopping list
@@ -42,33 +41,53 @@ def satisfy_shopping_list(shopping_list_json, inventory_json):
         current_score = find_score(store['inventory'], shopping_list_json)
         if current_score > 0:
             stores_with_scores[store['name']] = current_score
-    combo = find_store_combos(stores_with_scores, stores, shopping_list_json)
-    shopping_list_satisfiable = len(combo) > 0
+    combos = find_store_combos(stores_with_scores, stores, shopping_list_json)
+    shopping_list_satisfiable = len(combos) > 0
     if shopping_list_satisfiable:
         # print out number of stores and corresponding combinations
-        num_stores = len(combo) 
-        print "The shopping list can be satisfied by visiting {} store(s):".format(num_stores)
-        print_store_combination(combo)
+        for combination in combos:
+            num_stores = len(combination) 
+            print "The shopping list can be satisfied by visiting {} store(s):".format(num_stores)
+            print_store_combination(combination)
         pass
     else:
         print "No combination of given stores can satisfy this shopping list :("
         pass
-def find_store_combos(scores, stores, shopping_list):
-    combo = []
-    print shopping_list
-    for key in sorted(scores.items(), key=lambda x: x[1], reverse=True): #start with the highest scores first
-        for store in stores:
-            if is_shopping_list_satisfied(shopping_list):
-                break
-            elif key[0] == store['name']:
-                print shopping_list
-                if decrement_shopping_list(store['inventory'], shopping_list) == 1:
-                    combo.append(key[0])
-        if is_shopping_list_satisfied(shopping_list): break
-    print combo
-    print shopping_list
-    return combo
 
+def find_store_combos(scores, stores, shopping_list):
+    combo = list()
+    # max_score = len(shopping_list)*10 #maximum score, if this is in the list then we are done
+    # sorted_scores = sorted(scores.items(), key=lambda x: x[1], reverse=True)
+    # for store in sorted_scores:
+    #     if store[1] == max_score:
+    #         combo.add(store[0])
+    # if len(combo) > 0:
+    #     #we only have to visit the stores with the max score
+    #     return combo
+    for i in range(2, len(stores)):
+        test_set = [None]*i
+        find_combinations(stores, copy.deepcopy(shopping_list), test_set, 0, len(stores), 0, i, combo)
+    return combo
+def find_combinations(stores, shopping_list, data, start, end, index, subset_size, valid_combinations): #create an n-ary tree of combos and recurse up to find the shortest branch
+    if(index == subset_size):
+        should_add = True
+        shopping_list_copy = copy.deepcopy(shopping_list)
+        for k in range(subset_size):
+            should_add = decrement_shopping_list(data[k]['inventory'], shopping_list_copy)
+            if not should_add :
+                break
+        if should_add and is_shopping_list_satisfied(shopping_list_copy):
+            valid_combo_list = []
+            for d in data:
+                valid_combo_list.append(d['name']) 
+            valid_combinations.append(valid_combo_list)
+    i = start
+    while i < end and index < subset_size: 
+        data[index] = stores[i]
+        find_combinations(stores, shopping_list, data, i + 1, end, index + 1, subset_size, valid_combinations)
+        i = i + 1
+
+#Initial score based on the shopping list and inventory
 def find_score(store, shopping_list):
     score = 0
     for item in shopping_list:
@@ -80,14 +99,7 @@ def find_score(store, shopping_list):
     return score
 
 
-def compare_distance(satisfiable_list):
-    global minimum_number_to_visit
-    if not minimum_number_to_visit:
-        minimum_number_to_visit = copy.deepcopy(satisfiable_list)
-        return
-    if len(satisfiable_list) < len(minimum_number_to_visit):
-        minimum_number_to_visit = copy.deepcopy(satisfiable_list)
-    
+#Check if the shopping list is empty
 def is_shopping_list_satisfied(shopping_list_json):
     for key in shopping_list_json:
         if shopping_list_json[key] != 0:
@@ -96,17 +108,16 @@ def is_shopping_list_satisfied(shopping_list_json):
 
 
 def decrement_shopping_list(inventory, shopping_list):
-    score = -1
+    score = False
     for item in shopping_list:
         if item in inventory and shopping_list[item] > 0:
             if inventory[item] >= shopping_list[item]:
-                score = 1
+                score = True
                 shopping_list[item] = 0
             elif inventory[item] < shopping_list[item]:
-                score = 0
+                score = False
                 shopping_list[item] = shopping_list[item] = inventory[item]
     return score
-
 
 
 def print_store_combination(store_combination):
